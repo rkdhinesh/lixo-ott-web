1fb2b7b818f59ad8d7f0b9ab15362534681e5f70
karthidk27 <130198626+karthidk27@users.noreply.github.com>
2024-04-07 13:21:20 +0530

    Add files via upload

---
+++ src/environments/environment.natesh.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  appName: "movie-reservation-web",
+  production: true,
+  env: "prod",
+  baseUrl: '/services',
+  companyId: "61", // Obtained from DB
+  defaultLocation: 'Thiruvarur',
+  systemId: 'moviepanda',
+  venue: false
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  appName: "movie-reservation-web",
+  production: true,
+  env: "prod",
+  baseUrl: '/services',
+  companyId: "-999", // Obtained from DB
+  defaultLocation: undefined,
+  systemId: 'moviepanda',
+  venue: true
+};
